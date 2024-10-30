4a8dace592731822852f4fef4e98cfe5124c4635
vzalxndr <w69953@student.wsiz.edu.pl>
2024-10-30 17:18:00 +0100

    Add project files.

---
+++ lab_2/Constructions/Construction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2.Constructions
+{
+    internal class Construction
+    {
+        private string _buildMaterial;
+        public Construction() { }
+
+        public Construction(float height, float width, int entrances, int humanCapacity, string buildMaterial)
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            BuildMaterial = buildMaterial;
+        }
+
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+
+        public string BuildMaterial
+        {
+            get => _buildMaterial;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(value), "Build material cannot be empty.");
+                }
+                _buildMaterial = value;
+            }
+        }
+    }
+}
---
+++ lab_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using lab_2.Constructions;
+
+class Program
+{
+    static void Main()
+    {
+        var construction = new Construction(29.7f, 45.4f, 5, 124, "Material");
+
+        Console.WriteLine("Height: " + construction.Height);
+        Console.WriteLine("Width: " + construction.Width);
+        Console.WriteLine("Entrances: " + construction.Entrances);
+        Console.WriteLine("Human Capacity: " + construction.HumanCapacity);
+        Console.WriteLine("Build Material: " + construction.BuildMaterial);
+
+        try
+        {
+            construction.BuildMaterial = "";
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine("Exception caught: " + ex.Message);
+        }
+    }
+}
